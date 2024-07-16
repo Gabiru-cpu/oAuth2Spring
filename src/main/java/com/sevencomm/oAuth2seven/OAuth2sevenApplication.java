99d2e1edfdf32d0d6b534395a90860faacd073af
Gabriel Librande <gabriel_librande@hotmail.com>
2024-07-16 15:42:06 -0300

    first commit

---
+++ src/main/java/com/sevencomm/oAuth2seven/OAuth2sevenApplication.java
@@ -0,0 +1,13 @@
+package com.sevencomm.oAuth2seven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2sevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2sevenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevencomm/oAuth2seven/OAuth2sevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevencomm.oAuth2seven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2sevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
